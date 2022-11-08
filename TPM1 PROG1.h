e7d56933b59043e82bddbf8946aff2526bc1d62a
CSS-Muad-Dib <95708234+CSS-Muad-Dib@users.noreply.github.com>
2022-11-08 16:38:04 -0300

    Add files via upload

---
+++ TPM1 PROG1.h
@@ -0,0 +1,67 @@
+#ifndef TPM1_PROG1_H_INCLUDED
+#define TPM1_PROG1_H_INCLUDED
+
+
+typedef struct {
+prenda vipd;
+struct nodo *puntero;
+}nodo;
+
+
+typedef struct {
+nodo *acceso;
+nodo *cursor;
+nodo *cursor_aux;
+}lista;
+
+
+void init (lista *L){
+(*L).acceso=NULL;
+(*L).cursor=NULL;
+(*L).cursor_aux=NULL;
+}
+
+int insenty (lista *L){
+return(if L.acceso ==NULL)}
+
+int copy (lista *L){
+return(L.cursor)}
+
+void insert (lista *L, prenda elemento){
+nodo *nuevo;
+nuevo=(nodo*)malloc(sizeof(nodo));
+nuevo.vipd=elemento;
+if ((*L).acceso==(*L).cursor){
+    (*L).acceso=nuevo;
+    nuevo.puntero=(*L).cursor;
+    (*L).cursor=(*L).acceso;
+    (*L).cursor_aux=(*L).acceso }
+else {
+        (*L).cursor_aux=nuevo;
+        nuevo.puntero=(*L).cursor;
+        (*L).cursor=nuevo;
+}
+
+}
+
+void reset (lista *L){
+(*L).cursor=(*L).acceso;
+(*L).cursor_aux=(*L).acceso;
+}
+
+void forward (lista *L){
+(*L).cursor_aux=(*L).cursor;
+(*L).cursor=(*L).cursor;
+}
+
+void supress (lista *L){
+
+}
+
+
+
+
+
+
+
+#endif // TPM1_PROG1_H_INCLUDED
